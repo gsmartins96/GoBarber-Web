4fac8572ad9ceb4999db956ff4f4fd4fca7e2060
Gabriel dos Santos Martins <gsmartins96@hotmail.com>
2020-04-24 19:17:43 -0300

    :construction: configure time to list appointments

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,13 +1,51 @@
-import React, { useState, useMemo } from 'react';
-import { format, addDays, subDays } from 'date-fns';
+import React, { useState, useMemo, useEffect } from 'react';
+import {
+  format,
+  addDays,
+  subDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  isBefore,
+  isEqual,
+  parseISO,
+} from 'date-fns';
 import pt from 'date-fns/locale/pt';
+import { utcToZonedTime } from 'date-fns-tz';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import { Container, Time } from './styles';
 
 import api from '~/services/api';
 
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
 export default function Dashboard() {
   const [date, setDate] = useState(new Date());
+  const [schedule, setSchedule] = useState([]);
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get('schedule', {
+        params: { date },
+      });
+
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+      const data = range.map((hour) => {
+        const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+        const compareDate = utcToZonedTime(checkDate, timezone);
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.find((a) =>
+            isEqual(parseISO(a.date), compareDate)
+          ),
+        };
+      });
+
+      setSchedule(data);
+    }
+  }, [date]);
 
   const dateFormatted = useMemo(
     () => format(date, "d 'de' MMMM", { locale: pt }),
